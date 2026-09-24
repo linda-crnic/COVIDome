8d6c70980000fd15cff8175215626baaa15cea6a
kbartsch <kyleb83@gmail.com>
2026-09-24 16:07:10 -0600

    update cytokines to use secrets, update deps.

--- Cytokines/globals/applicationConfig.R
+++ Cytokines/globals/applicationConfig.R
@@ -1,3 +1,8 @@
+library(config)
+library(cyphr)
+library(sodium)
+library(base64enc)
+
 # suppress warnings  
 storeWarn <- getOption("warn")
 options(warn = -1)
@@ -5,7 +10,20 @@ options(warn = -1)
 # get App metadata
 appConfig <- config::get(file = "config/config.yml","appConfig") 
 
-conn_args <- config::get(file = "config/config.yml", "dataconnection")
+dataConnectionConfig <- config::get(file = "config/config.yml", "dataconnection")
+
+key <- cyphr::key_sodium(base64enc::base64decode(dataConnectionConfig$db_key))
+
+conn_args <- jsonlite::fromJSON(
+  cyphr::decrypt_string(base64enc::base64decode(dataConnectionConfig$db_connection_string), key)
+)
+
+conn_args$driver <- dataConnectionConfig$driver
+if (!is.null(dataConnectionConfig$TDS_Version)) {
+  conn_args$TDS_Version <- dataConnectionConfig$TDS_Version
+}
+
+rm(dataConnectionConfig, key)
 
 isProductionApp <- ifelse(appConfig$Environment=="Production",TRUE,FALSE)
 
@@ -23,12 +41,12 @@ dropdownlinks <- read_tsv("./config/dropdownlinks.tsv",col_types = cols())
 
 tutorials <- read_tsv("./config/tutorials.tsv",col_types = cols())
 
-plotlyCustomIcons <- readRDS('./config/plotlycustomicons.rds')
+plotlyCustomIcons <- readRDS('config/plotlycustomicons.rds')
 
 statTests <- CUSOMShinyHelpers::getStatTestByKeyGroup.methods
 
 adjustmentMethods <- c("none","Bonferroni","Benjamini-Hochberg (FDR)")
 
-
+pValueThreshold <- 0.05
 
 
